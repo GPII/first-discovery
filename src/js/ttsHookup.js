aacec575a7744a57d46fc449235da54a19bcc2c6
Justin Obara <obarajustin@gmail.com>
2015-02-12 15:24:44 -0500

    FLOE-269: press 'h' to read panel instructions

--- src/js/ttsHookup.js
+++ src/js/ttsHookup.js
@@ -50,10 +50,19 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
                         speakPanelMessage: {
                             funcName: "gpii.firstDiscovery.tts.fdHookup.speakPanelMessage",
                             args: ["{firstDiscoveryEditor}", "{that}.msgLookup.panelMsg", "{that}.queueSpeech"]
+                        },
+                        speakPanelInstructions: {
+                            funcName: "gpii.firstDiscovery.tts.fdHookup.speakPanelInstructions",
+                            args: ["{firstDiscoveryEditor}", "{that}.queueSpeech"]
                         }
                     },
                     listeners: {
-                        "onCreate.readPanel": "{that}.speakPanelMessage"
+                        "onCreate.readPanel": "{that}.speakPanelMessage",
+                        "onCreate.bindKeypress": {
+                            listener: "gpii.firstDiscovery.tts.fdHookup.bindKeypress",
+                            // 104 === 'h'
+                            args: ["body", 104, "{that}.speakPanelInstructions"]
+                        }
                     },
                     modelListeners: {
                         "{firstDiscoveryEditor}.model.currentPanelNum": "{that}.speakPanelMessage"
@@ -64,6 +73,14 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         panelInstructionsSelector: ".gpiic-fd-instructions"
     });
 
+    gpii.firstDiscovery.tts.fdHookup.bindKeypress = function (elm, keyCode, func) {
+        $(elm).keypress(function (e) {
+            if (e.keyCode === keyCode) {
+                func();
+            }
+        });
+    };
+
     gpii.firstDiscovery.tts.fdHookup.speakPanelMessage = function (that, template, speakFn) {
         var currentPanelNum = that.model.currentPanelNum;
         var msg = fluid.stringTemplate(template, {
@@ -71,6 +88,12 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             numPanels: that.panels.length,
             instructions: that.panels.eq(currentPanelNum - 1).find(that.options.panelInstructionsSelector).text()
         });
+        speakFn(msg);
+    };
+
+    gpii.firstDiscovery.tts.fdHookup.speakPanelInstructions = function (that, speakFn) {
+        var msg = that.panels.eq(that.model.currentPanelNum - 1).find(that.options.panelInstructionsSelector).text();
+
         speakFn(msg);
     };
 
