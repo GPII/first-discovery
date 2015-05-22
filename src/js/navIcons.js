09de3b6dfeb5be5f007a6ffeb2dd6c3a341b7f72
Antranig Basman <antranig.basman@colorado.edu>
2015-05-22 19:46:49 +0100

    FLOE-344: First working implementation of paging navIcons

--- src/js/navIcons.js
+++ src/js/navIcons.js
@@ -47,12 +47,15 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             that.locate("confirmedIndicator").addClass(that.options.styles.show);
         }
     };
-
+    
     /*
      * The navigation icons: the wrapper component to help determine the position of each nav icon.
      */
     fluid.defaults("gpii.firstDiscovery.navIcons", {
         gradeNames: ["fluid.viewRelayComponent", "autoInit"],
+        pageSize: 5,
+        iconWidth: "6rem",
+        iconHoles: [2], // a list of all the panel positions which have no nav icons (currently the "welcome" page)
         dynamicComponents: {
             icon: {
                 createOnEvent: "onCreateIcon",
@@ -70,8 +73,25 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
                 }
             }
         },
+        modelRelay: {
+            source: "currentPanelNum",
+            target: "pageNum",
+            singleTransform: {
+                type: "fluid.transforms.free",
+                args: [
+                    "{that}.model.currentPanelNum",
+                    "{that}.options.pageSize",
+                    "{that}.options.iconHoles"
+                ],
+                func: "gpii.firstDiscovery.navIcons.indexToPage"
+            }
+        },
+        modelListeners: {
+            pageNum: "gpii.firstDiscovery.navIcons.showPage({change}.value, {that}.options.pageSize, {that}.options.iconWidth, {that}.dom.pager)"
+        },
         selectors: {
-            icon: ".gpiic-fd-navIcon"
+            icon: ".gpiic-fd-navIcon",
+            pager: ".gpii-fd-navIcon-inner"
         },
         events: {
             onCreateIcon: null
@@ -80,6 +100,34 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             "onCreate.createIcons": "gpii.firstDiscovery.navIcons.createIcons"
         }
     });
+    
+    /** @param measure {String} A CSS measure as a string, consisting of a numeric dimension followed by units, such as "6rem"
+     * @return {Object} A parsed representation of the measure, holding the dimension in field <code>dimension</code> and the units in field <code>units</code>
+     */
+    gpii.parseCSSMeasure = function (measure) {
+        // Regular expression taken from http://stackoverflow.com/questions/2671427/parsing-css-measures
+        var parts = measure.match(/^([+-]?(?:\d+|\d*\.\d+))([a-z]*|%)$/);
+        return {
+            dimension: Number(parts[1]),
+            units: parts[2]
+        };
+    };
+    
+    gpii.firstDiscovery.navIcons.indexToPage = function (panelNum, pageSize, holes) {
+        var pastHoles = 0;
+        for (var i = 0; i < holes.length; ++ i) {
+            if (panelNum >= holes[i]) {
+                ++ pastHoles;
+            }
+        }
+        return Math.floor((panelNum - 1 - pastHoles) / pageSize);
+    };
+    
+    gpii.firstDiscovery.navIcons.showPage = function (pageNum, pageSize, iconWidth, pagerElement) {
+        var parsedWidth = gpii.parseCSSMeasure(iconWidth);
+        var newLeft = - parsedWidth.dimension * pageNum * pageSize;
+        pagerElement.css("left", newLeft + parsedWidth.units);
+    };
 
     gpii.firstDiscovery.navIcons.createIcons = function (that) {
         var icons = that.locate("icon");
