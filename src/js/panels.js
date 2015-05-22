a6a67dc97b73a785b701dae06b6ec2e08ba1b2ec
Simon Bates <simon@bitstructures.com>
2015-05-22 16:14:56 -0400

    FLOE-333: Update styling for new language panel structure

--- src/js/panels.js
+++ src/js/panels.js
@@ -549,7 +549,8 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
     });
 
     gpii.firstDiscovery.panel.lang.buildLangOptionsMarkup = function (langNames, langCodes) {
-        var template = "<div class=\"gpiic-fd-lang-row selectable\" role=\"option\" aria-selected=\"false\" data-lang=\"%langCode\">%langName</div>";
+        // TODO move this template somewhere better
+        var template = "<div class=\"gpiic-fd-lang-row selectable gpii-fd-choice\" role=\"option\" aria-selected=\"false\" data-lang=\"%langCode\"><span class=\"gpii-fd-indicator gpii-fd-icon\"></span><span class=\"gpii-fd-lang-label gpii-fd-choice-label\">%langName</span></div>";
         var markup = "";
         for (var i=0; i < langNames.length; i++) {
             var langName = langNames[i];
