9ebec2b5c8e3ca880e8469486894846b26131741
Cindy Qi Li <cli@ocadu.ca>
2015-09-28 10:22:10 -0400

    FLOE-422: Added a license file and updated license links in source code files.

--- src/js/navIcons.js
+++ src/js/navIcons.js
@@ -6,8 +6,8 @@ Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
 Licenses.
 
-You may obtain a copy of the ECL 2.0 License and BSD License at
-https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+You may obtain a copy of the License at
+https://github.com/fluid-project/first-discovery/raw/master/LICENSE.txt
 */
 
 (function ($, fluid) {
--- tests/js/navButtonsTests.js
+++ tests/js/navButtonsTests.js
@@ -5,7 +5,7 @@ Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
 You may obtain a copy of the License at
-https://github.com/gpii/universal/LICENSE.txt
+https://github.com/fluid-project/first-discovery/raw/master/LICENSE.txt
 */
 
 (function ($, fluid) {
