8f90807fdc69dd03ba1a22985ed5c95a140df064
Justin Obara <obarajustin@gmail.com>
2015-10-26 13:10:35 -0400

    FLOE-429: Updated license and package.json for GPII

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
 You may obtain a copy of the License at
-https://github.com/fluid-project/first-discovery/raw/master/LICENSE.txt
+https://raw.githubusercontent.com/GPII/first-discovery/master/LICENSE.txt
 */
 
 // Declare dependencies
