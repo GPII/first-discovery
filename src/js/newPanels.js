090e5c204283932efb2dd5765812799566bc11e3
John Workmeister <jtworkme@us.ibm.com>
2016-01-20 10:51:56 -0500

    initial

---
+++ src/js/newPanels.js
@@ -0,0 +1,12 @@
+(function ($, fluid){
+    "use strict";
+    fluid.defualts("gpii.firstDiscovery.panel.font",{
+        gradeName: ["gpii.firstDiscovery.panel.yesNo"],
+        preferenceMap:{
+            "gpii.firstDiscovery.font":{
+                "model.value" : "default"
+            }
+        }
+    });
+
+})(jQuery, fluid_2_0);
